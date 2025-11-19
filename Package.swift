b1eb3da3e3ed86e01ba305ba1dc2a73bd3e7b969
Mark Lawley <mark.lawley@gmail.com>
2025-11-19 11:40:49 -0500

    Add WebRTC dependency

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "openai-realtime-api-connector",
             targets: ["openai-realtime-api-connector"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/webrtc-sdk/webrtc", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
